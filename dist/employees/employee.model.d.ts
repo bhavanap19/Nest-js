7b1160d145e2c83fefc53c7c8093e9f937ce8b5c
bhavanap19 <69037140+bhavanap19@users.noreply.github.com>
2021-07-19 20:21:02 +0530

    Add files via upload

---
+++ dist/employees/employee.model.d.ts
@@ -0,0 +1,8 @@
+export declare class Employee {
+    id: string;
+    firstname: string;
+    lastname: string;
+    companyname: string;
+    contact: number;
+    constructor(id: string, firstname: string, lastname: string, companyname: string, contact: number);
+}
---
+++ dist/employees/employee.model.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Employee = void 0;
+class Employee {
+    constructor(id, firstname, lastname, companyname, contact) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.companyname = companyname;
+        this.contact = contact;
+    }
+}
+exports.Employee = Employee;
+//# sourceMappingURL=employee.model.js.map
